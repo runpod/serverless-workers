320e28278f6dc144838a966bc7d527b6e5ebad7c
Justin Merrell <merrelljustin@gmail.com>
2023-01-25 20:51:58 -0500

    remove repaint

--- StableDiffusion-v1/cog_example/predict.py
+++ StableDiffusion-v1/cog_example/predict.py
@@ -5,8 +5,7 @@ import torch
 from diffusers import (
     StableDiffusionPipeline,
     StableDiffusionImg2ImgPipeline,
-    StableDiffusionInpaintPipeline,
-    # StableDiffusionInpaintPipelineLegacy,
+    StableDiffusionInpaintPipelineLegacy,
 
     DDIMScheduler,
     DDPMScheduler,
@@ -21,7 +20,7 @@ from diffusers import (
     KDPM2DiscreteScheduler,
     # KarrasVeScheduler,
     PNDMScheduler,
-    RePaintScheduler,
+    # RePaintScheduler,
     # ScoreSdeVeScheduler,
     # ScoreSdeVpScheduler,
     UnCLIPScheduler,
@@ -55,7 +54,7 @@ class Predictor(BasePredictor):
             safety_checker=self.txt2img_pipe.safety_checker,
             feature_extractor=self.txt2img_pipe.feature_extractor,
         ).to("cuda")
-        self.inpaint_pipe = StableDiffusionInpaintPipeline(
+        self.inpaint_pipe = StableDiffusionInpaintPipelineLegacy(
             vae=self.txt2img_pipe.vae,
             text_encoder=self.txt2img_pipe.text_encoder,
             tokenizer=self.txt2img_pipe.tokenizer,
@@ -195,7 +194,7 @@ def make_scheduler(name, config):
         "KDPM2-D": KDPM2DiscreteScheduler.from_config(config),
         # "KARRAS-VE": KarrasVeScheduler.from_config(config),
         "PNDM": PNDMScheduler.from_config(config),
-        "RE-PAINT": RePaintScheduler.from_config(config),
+        # "RE-PAINT": RePaintScheduler.from_config(config),
         # "SCORE-VE": ScoreSdeVeScheduler.from_config(config),
         # "SCORE-VP": ScoreSdeVpScheduler.from_config(config),
         "UN-CLIPS": UnCLIPScheduler.from_config(config),
