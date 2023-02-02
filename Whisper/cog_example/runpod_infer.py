8b45dea29c29200354d3028160c7fb91f08788d1
Justin Merrell <merrelljustin@gmail.com>
2023-02-01 20:11:23 -0500

    fix: syntax

--- Whisper/cog_example/runpod_infer.py
+++ Whisper/cog_example/runpod_infer.py
@@ -92,25 +92,25 @@ def run(job):
     # Setting the float parameters
     job_input['temperature'] = float(job_input.get('temperature', 0))
     job_input['patience'] = float(job_input.get('patience', 0))
-    job_input['length_penalty'] = float(job_input.get('length_penalty', 0)
-    job_input['temperature_increment_on_fallback']=float(
+    job_input['length_penalty'] = float(job_input.get('length_penalty', 0))
+    job_input['temperature_increment_on_fallback'] = float(
         job_input.get('temperature_increment_on_fallback', 0.2)
     )
-    job_input['compression_ratio_threshold']=float(
+    job_input['compression_ratio_threshold'] = float(
         job_input.get('compression_ratio_threshold', 2.4)
     )
-    job_input['logprob_threshold']=float(job_input.get('logprob_threshold', -1.0))
-    job_input['no_speech_threshold']=0.6
+    job_input['logprob_threshold'] = float(job_input.get('logprob_threshold', -1.0))
+    job_input['no_speech_threshold'] = 0.6
 
     # Input validation
-    input_errors=validator.validate(job_input, INPUT_VALIDATIONS)
+    input_errors = validator.validate(job_input, INPUT_VALIDATIONS)
 
     if input_errors:
         return {"error": input_errors}
 
-    job_input['audio']=download.download_input_objects([job_input['audio']])[0]
+    job_input['audio'] = download.download_input_objects([job_input['audio']])[0]
 
-    whisper_results=MODEL.predict(
+    whisper_results = MODEL.predict(
         audio=job_input["audio"],
         model=job_input.get("model", 'base'),
         transcription=job_input.get('transcription', 'plain_text'),
