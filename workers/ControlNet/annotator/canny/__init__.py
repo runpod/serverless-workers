91736078f3c4e14d3482f1a0fae769583f8bc5b8
Justin Merrell <merrelljustin@gmail.com>
2023-03-15 09:55:28 -0400

    Update __init__.py

--- workers/ControlNet/annotator/canny/__init__.py
+++ workers/ControlNet/annotator/canny/__init__.py
@@ -1,6 +1,12 @@
 import cv2
 
 
-class CannyDetector:
-    def __call__(self, img, low_threshold, high_threshold):
-        return cv2.Canny(img, low_threshold, high_threshold)
+def apply_canny(img, low_threshold, high_threshold):
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+# import cv2
+
+
+# class CannyDetector:
+#     def __call__(self, img, low_threshold, high_threshold):
+#         return cv2.Canny(img, low_threshold, high_threshold)
