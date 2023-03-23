d63d4fe22754d7e9c3a9efe8975e6f3c0e697c77
Justin Merrell <merrelljustin@gmail.com>
2023-03-22 20:26:55 -0400

    Update runpod_infer.py

--- workers/huggingface-transformers/runpod_infer.py
+++ workers/huggingface-transformers/runpod_infer.py
@@ -2,28 +2,61 @@
 RunPod | Transformer | Handler
 '''
 import runpod
+from runpod.serverless.utils.rp_validator import validate
+
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer
 
 model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
 tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
+
+INPUT_SCHEMA = {
+    'prompt': {
+        'type': str,
+        'required': True
+    },
+    'do_sample': {
+        'type': bool,
+        'required': False,
+        'default': True,
+        'description': '''
+            Enables decoding strategies such as multinomial sampling,
+            beam-search multinomial sampling, Top-K sampling and Top-p sampling.
+            All these strategies select the next token from the probability distribution
+            over the entire vocabulary with various strategy-specific adjustments.
+        '''
+    },
+    'max_length': {
+        'type': int,
+        'required': False,
+        'default': 100
+    },
+    'temperature': {
+        'type': float,
+        'required': False,
+        'default': 0.9
+    }
+}
 
 
 def generator(job):
     '''
     Run the job input to generate text output.
     '''
-    job_input = job['input']
+    # Validate the input
+    val_input = validate(job['input'], INPUT_SCHEMA)
+    if 'errors' in val_input:
+        return {"error": val_input['errors']}
+    val_input = val_input['validated_input']
 
-    prompt = job_input['prompt']
-
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+    input_ids = tokenizer(val_input['prompt'], return_tensors="pt").input_ids
 
     gen_tokens = model.generate(
         input_ids,
-        do_sample=True,
-        temperature=0.9,
-        max_length=100,
+        do_sample=val_input['do_sample'],
+        temperature=val_input['temperature'],
+        max_length=val_input['max_length'],
     )
+
     gen_text = tokenizer.batch_decode(gen_tokens)[0]
 
     return gen_text
