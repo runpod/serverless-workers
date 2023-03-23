42cb634208cd3480075a2401375839e8af29a494
Justin Merrell <merrelljustin@gmail.com>
2023-03-22 23:53:22 -0400

    Update model_fetcher.py

--- workers/huggingface-transformers/model_fetcher.py
+++ workers/huggingface-transformers/model_fetcher.py
@@ -16,22 +16,6 @@ def download_model(model_name):
         tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
         model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neox-20b")
 
-    prompt = (
-        "In a shocking finding, scientists discovered a herd of unicorns living in a remote, "
-        "previously unexplored valley, in the Andes Mountains. Even more surprising to the "
-        "researchers was the fact that the unicorns spoke perfect English."
-    )
-
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-
-    gen_tokens = model.generate(
-        input_ids,
-        do_sample=True,
-        temperature=0.9,
-        max_length=100,
-    )
-    gen_text = tokenizer.batch_decode(gen_tokens)[0]
-
 
 # ---------------------------------------------------------------------------- #
 #                                Parse Arguments                               #
