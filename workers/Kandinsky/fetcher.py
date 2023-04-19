f3861bd49a7fae20417d9665e342cba6d1597297
Justin Merrell <merrelljustin@gmail.com>
2023-04-19 11:53:34 -0400

    Update fetcher.py

--- workers/Kandinsky/fetcher.py
+++ workers/Kandinsky/fetcher.py
@@ -1,2 +1,2 @@
 from kandinsky2 import get_kandinsky2
-model = get_kandinsky2('cuda', task_type='text2img', model_version='2.1', use_flash_attention=False)
+get_kandinsky2(task_type='text2img', model_version='2.1', use_flash_attention=False)
