484ea2b0e61b78431559517fd03fb6515de93623
Justin Merrell <merrelljustin@gmail.com>
2023-04-19 12:12:23 -0400

    Update fetcher.py

--- workers/Kandinsky/fetcher.py
+++ workers/Kandinsky/fetcher.py
@@ -1,3 +1,6 @@
 from kandinsky2 import get_kandinsky2
-get_kandinsky2('cpu', task_type='text2img', model_version='2.1',
-               use_flash_attention=False, cache_dir="/kandinsky2",)
+try:
+    get_kandinsky2('cuda', task_type='text2img', model_version='2.1',
+                   use_flash_attention=False, cache_dir="/kandinsky2",)
+except RuntimeError as err:
+    print(err)
